2acd23d39d487d86bbc334a3dd3f04937ac0a7b3
Zoe Barnswell <zob@princeton.edu>
2019-05-12 17:57:27 -0400

    oooo she cute

--- main.js
+++ main.js
@@ -5,42 +5,80 @@
 		var height = window.innerHeight;
 
 		var scene = new Physijs.Scene;
+		//scene.setGravity(new THREE.Vector3( 0, -10, 0));
 		
 		scene.addEventListener( 'update', function() {
 			//your code. physics calculations have done updating
 		});
 		
-	    var ground_material = Physijs.createMaterial(
-			new THREE.MeshPhongMaterial( { color: 0x008080, flatShading: true } ),0, .9 // low restitution
-		);
 		// Playground
-		var ground = new Physijs.BoxMesh(new THREE.BoxGeometry(2000, 1, 2000),ground_material,0 // mass
+		var grass = new THREE.TextureLoader().load('../textures/grass.png');
+		grass.mapping = THREE.EquirectangularReflectionMapping;
+	    var ground_material = Physijs.createMaterial(
+			new THREE.MeshPhongMaterial( { color: 0x008080, flatShading: true, map: grass } ),0, .9 // low restitution
+		);
+
+		var ground = new Physijs.BoxMesh(new THREE.BoxGeometry(4000, 1, 4000), ground_material, 0 // mass
 		);
 		ground.receiveShadow = true;
-		ground.position.x -=450;
+		ground.position.x -= 500;
 		scene.add(ground);
 
+		// Walls
+		var sky = new THREE.TextureLoader().load('../textures/sky.png');
+		sky.mapping = THREE.EquirectangularReflectionMapping;
+	    var wall_material = Physijs.createMaterial(
+			new THREE.MeshPhongMaterial( { flatShading: true, map: sky } ),0, .9 // low restitution
+		);
+
+	    // Back Wall
+		var back = new Physijs.BoxMesh(new THREE.BoxGeometry(4000, 3000, 1), wall_material, 0);
+		back.receiveShadow = true;
+		back.position.x -= 500;
+		back.position.y += 1500;
+		back.position.z -= 2000;
+		scene.add(back);
+
+		// Left Wall
+		var left = new Physijs.BoxMesh(new THREE.BoxGeometry(1, 3000, 4000), wall_material, 0);
+		left.receiveShadow = true;
+		left.position.y += 1500;
+		left.position.x += 1500;
+		scene.add(left);
+
+		// Right Wall
+		// Backwards :/
+		var right = new Physijs.BoxMesh(new THREE.BoxGeometry(1, 3000, 4000), wall_material, 0);
+		right.receiveShadow = true;
+		right.position.y += 1500;
+		right.position.x -= 2500;
+		scene.add(right);
 
 		// Machine Space
+		var sand = new THREE.TextureLoader().load('../textures/sand.png');
+		sand.mapping = THREE.EquirectangularReflectionMapping;
+
 		var ground_material2 = Physijs.createMaterial(
-			new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } ),0, .9 // low restitution
+			new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true, map: sand } ),0, .9 // low restitution
 		);
 		var machineSpace = new Physijs.BoxMesh(new THREE.BoxGeometry(400, 1, 750),ground_material2,0 // mass
 		);
 		machineSpace.receiveShadow = true;
 		machineSpace.position.y += 10;
-		machineSpace.position.x +=250;
+		machineSpace.position.x += 850;
 		scene.add(machineSpace);
 
 	    var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
 
-	    camera.position.y = 160;
-		camera.position.z = 400;
+	    camera.position.y = 160; 
+		camera.position.z = 400; 
 
 		scene.add(camera);
 
 	    var renderer = new THREE.WebGLRenderer({ antialias: true });
 		renderer.setSize(width, height);
+		renderer.gammaInput = true;
+		renderer.gammaOutput = true;
 		document.body.appendChild(renderer.domElement);
 
 		var clock = new THREE.Clock;
@@ -78,10 +116,16 @@
 
 
 		//DOMINO FIRST thank you
+		var domino_texture = new THREE.TextureLoader().load('../textures/domino.png');
+		domino_texture.mapping = THREE.EquirectangularReflectionMapping;
+	    var domino_material = Physijs.createMaterial(
+			new THREE.MeshPhongMaterial( { flatShading: true, map: domino_texture } ),0, .9 // low restitution
+		);
+
 		for(var i = 0; i < 100; i++) {
-			var domino = new Physijs.BoxMesh(new THREE.BoxGeometry(70, 140, 35), material);
+			var domino = new Physijs.BoxMesh(new THREE.BoxGeometry(70, 140, 35), domino_material);
 			domino.position.y += 80;
-			domino.position.x += 150;
+			domino.position.x += 700;
 			domino.position.z -= 200;
 			domino.castShadow = true;
 			domino.receiveShadow = true;
@@ -92,10 +136,16 @@
 		
 
 		//ball next thank you
+		var ball_texture = new THREE.TextureLoader().load('../textures/ball.png');
+		ball_texture.mapping = THREE.SphericalReflectionMapping;
+		var ball_material = Physijs.createMaterial(
+			new THREE.MeshPhongMaterial( { flatShading: true, map: ball_texture } ),0, .9 // low restitution
+		);
+
 		for(var i = 0; i < 100; i++) {
-			var ball = new Physijs.SphereMesh(new THREE.SphereGeometry(30, 30, 30 ), material);
+			var ball = new Physijs.SphereMesh(new THREE.SphereGeometry(30, 30, 30 ), ball_material);
 			ball.position.y += 40;
-			ball.position.x += 150;
+			ball.position.x += 700;
 			ball.castShadow = true;
 			ball.receiveShadow = true;
 			scene.add(ball);
@@ -104,10 +154,11 @@
 
 		// Large Cube
 		for(var i = 0; i < 100; i++) {
-			var cube = new Physijs.BoxMesh(new THREE.BoxGeometry(120, 120, 120), material, 0);
+			var large = new THREE.MeshPhongMaterial({ color: 0xd368f9, flatShading: true });
+			var cube = new Physijs.BoxMesh(new THREE.BoxGeometry(120, 120, 120), large, 0);
 			cube.position.z -= 200;
 			cube.position.y += 70;
-			cube.position.x += 300;
+			cube.position.x += 850;
 			cube.castShadow = true;
 			cube.receiveShadow = true;
 			scene.add(cube);
@@ -116,9 +167,10 @@
 
 		// Medium Cube
 		for(var i = 0; i < 100; i++) {
-			var cube = new Physijs.BoxMesh(new THREE.BoxGeometry(60, 60, 60), material, 0);
+			var medium = new THREE.MeshPhongMaterial({ color: 0xf9e368, flatShading: true });
+			var cube = new Physijs.BoxMesh(new THREE.BoxGeometry(60, 60, 60), medium, 0);
 			cube.position.y += 40;
-			cube.position.x += 300;
+			cube.position.x += 850;
 			cube.castShadow = true;
 			cube.receiveShadow = true;
 			scene.add(cube);
@@ -127,10 +179,11 @@
 
 		// Small Cube
 		for(var i = 0; i < 100; i++) {
-			var cube = new Physijs.BoxMesh(new THREE.BoxGeometry(30, 30, 30), material, 0);
+			var small = new THREE.MeshPhongMaterial({ color: 0x3676f7, flatShading: true });
+			var cube = new Physijs.BoxMesh(new THREE.BoxGeometry(30, 30, 30), small, 0);
 			cube.position.z += 100;
 			cube.position.y += 25;
-			cube.position.x += 300;
+			cube.position.x += 850;
 			cube.castShadow = true;
 			cube.receiveShadow = true;
 			scene.add(cube);
@@ -138,9 +191,15 @@
 		}
 
 		// SeeSaw
-		var material = new THREE.MeshPhongMaterial({ color: 0xb76e79, flatShading: true });
-		var fulcrum = new Physijs.ConeMesh(new THREE.CylinderGeometry(10, 30, 30), material);
-		fulcrum.position.x += 150;
+		var wood_texture = new THREE.TextureLoader().load('../textures/wood.png');
+		wood_texture.mapping = THREE.SphericalReflectionMapping;
+		var wood_material = Physijs.createMaterial(
+			new THREE.MeshPhongMaterial( { flatShading: true, map: wood_texture } ),0, .9 // low restitution
+		);
+
+		var stand_color = new THREE.MeshPhongMaterial({ color: 0xef2354, flatShading: true });
+		var fulcrum = new Physijs.ConeMesh(new THREE.CylinderGeometry(10, 30, 30), stand_color);
+		fulcrum.position.x += 700;
 		fulcrum.position.y += 25;
 		fulcrum.position.z += 200;
 		fulcrum.castShadow = true;
@@ -148,14 +207,31 @@
 		scene.add(fulcrum);
 		objects.push(fulcrum);
 
-		var seesaw = new Physijs.BoxMesh(new THREE.BoxGeometry(200, 10, 70), material);
+		var seesaw = new Physijs.BoxMesh(new THREE.BoxGeometry(200, 10, 70), wood_material);
 		seesaw.position.y += 45;
-		seesaw.position.x += 145;
+		seesaw.position.x += 695;
 		seesaw.position.z += 200;
 		seesaw.castShadow = true;
 		seesaw.receiveShadow = true;
 		scene.add(seesaw);
 		objects.push(seesaw);
+
+		// SeeSaw Constraint
+		// var constraint = new Physijs.HingeConstraint(
+  //   		fulcrum, // First object to be constrained
+  //   		seesaw, // OPTIONAL second object - if omitted then physijs_mesh_1 will be constrained to the scene
+  //   		new THREE.Vector3( 0, 10, 0 ), // point in the scene to apply the constraint
+  //   		new THREE.Vector3( 1, 0, 0 ) // Axis along which the hinge lies - in this case it is the X axis
+		// );
+		// scene.addConstraint( constraint );
+		// constraint.setLimits(
+		//     low, // minimum angle of motion, in radians
+		//     high, // maximum angle of motion, in radians
+		//     bias_factor, // applied as a factor to constraint error
+		//     relaxation_factor, // controls bounce at limit (0.0 == no bounce)
+		// );
+		// constraint.enableAngularMotor( target_velocity, acceration_force );
+		// constraint.disableMotor();
 
 		// Inclined Plane
 		var material = new THREE.MeshPhongMaterial({ color: 0xb76e79, flatShading: true });
@@ -185,7 +261,7 @@
 		geometry.computeVertexNormals();
 
 		var inclinedPlane = new Physijs.ConcaveMesh(geometry, material);
-		inclinedPlane.position.x += 325;
+		inclinedPlane.position.x += 875;
 		inclinedPlane.position.y += 10;
 		inclinedPlane.position.z += 200;
 		scene.add(inclinedPlane);
@@ -198,50 +274,34 @@
 
   		// Pendulum
 		// Swing
-		var swingMass = 1.2;
-		var swingRadius = 50;
-		var swing = new Physijs.SphereMesh( new THREE.SphereGeometry( swingRadius, 20, 20 ), new THREE.MeshPhongMaterial( { color: 0x202020, flatShading: true } ) );
-		swing.castShadow = true;
-		swing.receiveShadow = true;
-		swing.position.set(150, 70, -200);
-		scene.add(swing);
-		objects.push(swing);
+		// var swingMass = 1.2;
+		// var swingRadius = 50;
+		// var swing = new Physijs.SphereMesh( new THREE.SphereGeometry( swingRadius, 20, 20 ), new THREE.MeshPhongMaterial( { color: 0x202020, flatShading: true } ) );
+		// swing.castShadow = true;
+		// swing.receiveShadow = true;
+		// swing.position.set(150, 70, -200);
+		// scene.add(swing);
+		// objects.push(swing);
 
 		// Stand
-		var base = new Physijs.BoxMesh(new THREE.BoxGeometry(10, 10, 70), material);
+		//var base = new Physijs.BoxMesh(new THREE.BoxGeometry(10, 10, 70), material);
 
 		
 		// Seesaw
-		var fulcrum = new Physijs.ConeMesh(new THREE.CylinderGeometry(10, 30, 30), material);
-		fulcrum.position.set(150, 15, 200);
-		fulcrum.castShadow = true;
-		fulcrum.receiveShadow = true;
-		scene.add(fulcrum);
-		objects.push(fulcrum);
+		// var fulcrum = new Physijs.ConeMesh(new THREE.CylinderGeometry(10, 30, 30), material);
+		// fulcrum.position.set(150, 15, 200);
+		// fulcrum.castShadow = true;
+		// fulcrum.receiveShadow = true;
+		// scene.add(fulcrum);
+		// objects.push(fulcrum);
 
-		var seesaw = new Physijs.BoxMesh(new THREE.BoxGeometry(200, 10, 70), material);
-		seesaw.position.(145, 35, 200);
-		seesaw.castShadow = true;
-		seesaw.receiveShadow = true;
-		scene.add(seesaw);
-		objects.push(seesaw);
+		// var seesaw = new Physijs.BoxMesh(new THREE.BoxGeometry(200, 10, 70), material);
+		// seesaw.position.set(145, 35, 200);
+		// seesaw.castShadow = true;
+		// seesaw.receiveShadow = true;
+		// scene.add(seesaw);
+		// objects.push(seesaw);
 
-		// SeeSaw Constraint
-		// var constraint = new Physijs.HingeConstraint(
-  //   		fulcrum, // First object to be constrained
-  //   		seesaw, // OPTIONAL second object - if omitted then physijs_mesh_1 will be constrained to the scene
-  //   		new THREE.Vector3( 0, 10, 0 ), // point in the scene to apply the constraint
-  //   		new THREE.Vector3( 1, 0, 0 ) // Axis along which the hinge lies - in this case it is the X axis
-		// );
-		// scene.addConstraint( constraint );
-		// constraint.setLimits(
-		//     low, // minimum angle of motion, in radians
-		//     high, // maximum angle of motion, in radians
-		//     bias_factor, // applied as a factor to constraint error
-		//     relaxation_factor, // controls bounce at limit (0.0 == no bounce)
-		// );
-		// constraint.enableAngularMotor( target_velocity, acceration_force );
-		// constraint.disableMotor();
 
 
 
