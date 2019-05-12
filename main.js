21fa3e354a70f271c4370f11a17ef8134f408a8e
Pelumi Odimayo <oodimayo@princeton.edu>
2019-05-12 17:36:54 -0400

    Transform controls + camera manipluation. Object rotation not working yet

--- main.js
+++ main.js
@@ -34,8 +34,8 @@
 
 	    var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
 
-	    camera.position.y = 160;
-		camera.position.z = 400;
+	    camera.position.y = 500;
+		camera.position.z = 1000;
 
 		scene.add(camera);
 
@@ -46,14 +46,11 @@
 		var clock = new THREE.Clock;
 
 		// Orbit controls
-		var controls = new THREE.OrbitControls(camera);
-		controls.enabled = true;
-		controls.enablePan = true;
-		controls.enableZoom = true;
-		controls.enableKeys = true;
+		var orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
+		orbitControls.enableKeys = false;
 
 		function render() {
-			controls.update();
+			orbitControls.update();
 			renderer.render(scene, camera);
     		requestAnimationFrame(render);
 		}
@@ -71,75 +68,63 @@
 		var light = new THREE.AmbientLight( 0x222222 );
 		scene.add( light );
 
-
-		// Lets add some shapes!
+		// All object
 		var objects = [];
 
-
 		//DOMINO FIRST thank you
-		for(var i = 0; i < 100; i++) {
-			var material = new THREE.MeshPhongMaterial({ color: 0xb76e79, flatShading: true });
-			var domino = new Physijs.BoxMesh(new THREE.BoxGeometry(70, 140, 35), material);
-			domino.position.y += 80;
-			domino.position.x += 150;
-			domino.position.z -= 200;
-			domino.castShadow = true;
-			domino.receiveShadow = true;
-			scene.add(domino);
-			objects.push(domino);
-		}
+		var material = new THREE.MeshPhongMaterial({ color: 0xb76e79, flatShading: true });
+		var domino = new Physijs.BoxMesh(new THREE.BoxGeometry(70, 140, 35), material);
+		domino.position.y += 80;
+		domino.position.x += 150;
+		domino.position.z -= 200;
+		domino.castShadow = true;
+		domino.receiveShadow = true;
+		scene.add(domino);
+		objects.push(domino);
 
 		
 
 		//ball next thank you
-		for(var i = 0; i < 100; i++) {
-			var material = new THREE.MeshPhongMaterial({ color: 0xb76e79, flatShading: true });
-			var ball = new Physijs.SphereMesh(new THREE.SphereGeometry(30, 30, 30 ), material);
-			ball.position.y += 40;
-			ball.position.x += 150;
-			ball.castShadow = true;
-			ball.receiveShadow = true;
-			scene.add(ball);
-			objects.push(ball);
-		}
+		var material = new THREE.MeshPhongMaterial({ color: 0xb76e79, flatShading: true });
+		var ball = new Physijs.SphereMesh(new THREE.SphereGeometry(30, 30, 30 ), material);
+		ball.position.y += 40;
+		ball.position.x += 150;
+		ball.castShadow = true;
+		ball.receiveShadow = true;
+		scene.add(ball);
+		objects.push(ball);
 
 		// Large Cube
-		for(var i = 0; i < 100; i++) {
-			var material = new THREE.MeshPhongMaterial({ color: 0xb76e79, flatShading: true });
-			var cube = new Physijs.BoxMesh(new THREE.BoxGeometry(120, 120, 120), material, 0);
-			cube.position.z -= 200;
-			cube.position.y += 70;
-			cube.position.x += 300;
-			cube.castShadow = true;
-			cube.receiveShadow = true;
-			scene.add(cube);
-			objects.push(cube);
-		}
+		var material = new THREE.MeshPhongMaterial({ color: 0xb76e79, flatShading: true });
+		var cube = new Physijs.BoxMesh(new THREE.BoxGeometry(120, 120, 120), material, 0);
+		cube.position.z -= 200;
+		cube.position.y += 70;
+		cube.position.x += 300;
+		cube.castShadow = true;
+		cube.receiveShadow = true;
+		scene.add(cube);
+		objects.push(cube);
 
 		// Medium Cube
-		for(var i = 0; i < 100; i++) {
-			var material = new THREE.MeshPhongMaterial({ color: 0xb76e79, flatShading: true });
-			var cube = new Physijs.BoxMesh(new THREE.BoxGeometry(60, 60, 60), material, 0);
-			cube.position.y += 40;
-			cube.position.x += 300;
-			cube.castShadow = true;
-			cube.receiveShadow = true;
-			scene.add(cube);
-			objects.push(cube);
-		}
+		var material = new THREE.MeshPhongMaterial({ color: 0xb76e79, flatShading: true });
+		var cubeM = new Physijs.BoxMesh(new THREE.BoxGeometry(60, 60, 60), material, 0);
+		cubeM.position.y += 40;
+		cubeM.position.x += 300;
+		cubeM.castShadow = true;
+		cubeM.receiveShadow = true;
+		scene.add(cubeM);
+		objects.push(cubeM);
 
 		// Small Cube
-		for(var i = 0; i < 100; i++) {
-			var material = new THREE.MeshPhongMaterial({ color: 0xb76e79, flatShading: true });
-			var cube = new Physijs.BoxMesh(new THREE.BoxGeometry(30, 30, 30), material, 0);
-			cube.position.z += 100;
-			cube.position.y += 25;
-			cube.position.x += 300;
-			cube.castShadow = true;
-			cube.receiveShadow = true;
-			scene.add(cube);
-			objects.push(cube);
-		}
+		var material = new THREE.MeshPhongMaterial({ color: 0xb76e79, flatShading: true });
+		var cubeS = new Physijs.BoxMesh(new THREE.BoxGeometry(30, 30, 30), material, 0);
+		cubeS.position.z += 100;
+		cubeS.position.y += 25;
+		cubeS.position.x += 300;
+		cubeS.castShadow = true;
+		cubeS.receiveShadow = true;
+		scene.add(cubeS);
+		objects.push(cubeS);
 
 		// SeeSaw
 		var material = new THREE.MeshPhongMaterial({ color: 0xb76e79, flatShading: true });
@@ -195,8 +180,21 @@
 		scene.add(inclinedPlane);
 		objects.push(inclinedPlane);
 
-		var dragcontrols = new THREE.DragControls(objects, camera, renderer.domElement);
-
+		for (var i = 0; i < objects.length; i++) {
+			if (i != 3) {
+				continue;
+			}
+			var transformControls = new THREE.TransformControls(camera, renderer.domElement);    
+			transformControls.addEventListener('change', render);
+			transformControls.attach(objects[i]);
+			transformControls.addEventListener('mouseDown', function () {
+				orbitControls.enabled = false;
+			});
+			transformControls.addEventListener('mouseUp', function () {
+				orbitControls.enabled = true;
+			});
+			scene.add(transformControls);
+		}
 		// scene.simulate();
 
 
@@ -243,10 +241,27 @@
 		swing.castShadow = true;
 		swing.receiveShadow = true;
 		scene.add(swing);
-		objects.push(swing);
 		
 
+		/* When the user clicks on the button, 
+		toggle between hiding and showing the dropdown content */
+		function myFunction() {
+			document.getElementById("myDropdown").classList.toggle("show");
+		}
 		
+		// Close the dropdown menu if the user clicks outside of it
+		window.onclick = function(event) {
+			if (!event.target.matches('.dropbtn')) {
+			var dropdowns = document.getElementsByClassName("dropdown-content");
+			var i;
+			for (i = 0; i < dropdowns.length; i++) {
+				var openDropdown = dropdowns[i];
+				if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+				}
+			}
+			}
+		}
 
 
 
