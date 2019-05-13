d1e96e5a4a6da9200dd3fae5a29f0fdc8490553f
Pelumi Odimayo <oodimayo@princeton.edu>
2019-05-13 15:07:57 -0400

    Figured out snap issue, the physics simulation handles all position updates. So we need a way for transform controls to be able to override the physics simulation.

--- gui.js
+++ gui.js
@@ -18,6 +18,10 @@ var InclinedPlane = function() {
 	this.inclined_plane = function() { generateInclinedPlane() };
 }
 
+var Simulate = function() {
+	this.simulate = function() { Simulation() };
+}
+
 function dropdown() {
 	var gui = new dat.GUI();
 	var domino = new Domino();
@@ -25,11 +29,13 @@ function dropdown() {
 	var cube = new Cube();
 	var seesaw = new SeeSaw();
 	var i_p = new InclinedPlane();
+	var simulate = new Simulate();
 	gui.add(domino, 'domino');
 	gui.add(ball, 'ball');
 	gui.add(cube, 'cube');
 	gui.add(seesaw, 'seesaw');
 	gui.add(i_p, 'inclined_plane');
+	gui.add(simulate, 'simulate');
 }
 
 window.onload = function() {
